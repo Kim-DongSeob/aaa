5513ca2be9493fb44c377bfc851b384249409337
Kim-DongSeob <imjambo0916@gmail.com>
2022-01-10 16:15:22 +0900

    asd

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+from flask import Flask, render_template, jsonify, request, redirect, session
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
+db = client.chapter01
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8080, debug=True)
